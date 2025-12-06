97325c6d2d70786609bee3f165b09488da98ec38
CardosoNascimento <cardoson52@gmail.com>
2025-12-06 14:31:58 -0300

    Esqueleto do programa montado.

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@
 int main (int narg, char *argv[]) {
 	int n, *ordem, i, k;
 	Jogador *j;
-	char letra;
+	char letra, buffer[100];
 	
 	printf("*** JOGO AMEDONHA ***\n");
 	
@@ -28,11 +28,42 @@ int main (int narg, char *argv[]) {
 		printf("A categoria desta rodada eh: \n");
 		printf("A ordem desta jogada sera: \n");
 		ordem = sortear_ordem(n);
+		
 		for (k = 0; k < n; k++) {
-			printf("  %d. %s\n", k + 1, j[ordem[k]]);
+			printf("  %d. %s\n", k + 1, j[ordem[k]].nome);
 		}
+		
+		printf("\nTecle [Enter] para iniciar a rodada: ");
+		fgets(buffer, 100, stdin);
+		limpa_tela();
+		
+		// Recebe as respostas de cada jogador
+		for (k = 0; k < n; k++) {
+			printf("%s, voce deve entra um ""Nome da categoria"" com letra ""%c"" em n segundos: ", j[ordem[k]].nome, letra);
+			fgets(buffer, 100, stdin);
+			limpa_tela();
+		}
+		
+		// Exibe as jogadas realizadas por cada jogador
+		printf("Jogadas realizadas:\n");
+		for (k = 0; k < n; k++)
+			printf("%s:    %s\n", j[ordem[k]].nome, "resposta"); 
+		
+		// Exibe a tabela de escores da rodada
+		printf("\nConcluida a rodada, esta eh a tabela de escores:\n");
+		
+		printf("--- aqui tabela de escores ---\n");
+		
+		printf("\nTecle [Enter] para iniciar a proxima rodada: ");
+		fgets(buffer, 100, stdin);
+		limpa_tela();
 	}
 	
+	printf("\nRESULTADO FINAL:\n");
+	
+	printf("--- aqui resultado final ---");
+	
+	printf("O ganhador eh: \n");
 	
 	return EXIT_SUCCESS;
 }
