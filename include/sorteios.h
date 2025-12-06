b1764015ed6e8cb72ccd2abd7286ad558d7664da
CardosoNascimento <cardoson52@gmail.com>
2025-12-06 19:48:35 -0300

    Adicionado previnição de include repetida de header

--- include/sorteios.h
+++ include/sorteios.h
@@ -1,3 +1,6 @@
+#ifndef SORTEIOS_H
+#define SORTEIOS_H
+
 // Muda a semente de geracao de numeros para que a sequencia seja diferente toda vez que o programa for rodado.
 void iniciar_semente();
 
@@ -6,3 +9,5 @@ char sortear_letra();
 
 // Sorteia a ordem de jogada dos n jogadores
 int *sortear_ordem(int n);
+
+#endif
